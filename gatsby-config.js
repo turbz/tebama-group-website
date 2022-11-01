f2ae945e324ffdbb92a009c21b6230837d84c2a9
turbz <thabisomoha@gmail.com>
2022-11-01 10:53:26 +0200

    adding github pages to gatsby website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  pathPrefix: "/tebama-group-website",
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.tebamagroup.co.za`,
   },
   plugins: [],
-}
+};
